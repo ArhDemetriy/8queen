a00ae99538ea9a70bdb296215e7637c8395dc4b2
Demetriy <ArhDemetriy@GMail.com>
2021-05-22 23:14:31 +0800

    add: all

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChessTable from "./chessTable/ChessTable";
+import Queen from "./queen/Queen";
 
 function App() {
+  const tables = (new Queen()).getQueens()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{
+      tables
+        .map(table => <ChessTable queenPositions={table} key={Math.random()} />)
+    }</div>
   );
 }
 
--- src/chessTable/ChessTable.tsx
+++ src/chessTable/ChessTable.tsx
@@ -1,9 +1,26 @@
-// import React from 'react';
+import React from 'react';
 import './ChessTable.css'
 
-class ChessTable {
-  constructor() { }
+export interface ChessTableProps{
+  queenPositions: number[]
+}
+export default function ChessTable(props: ChessTableProps) {
+  const table = [] as boolean[]
+
+  for (let i = 0; i < props.queenPositions.length; i++) {
+    for (const position of props.queenPositions) {
+      table.push(i == position)
+    }
+  }
+
+  return <div className='ChessTable'>{
+    table.map(point => <div
+      className={`ChessTable__item ${point ? 'ChessTable__item--queen' : ''}`}
+      key={Math.random()} >{
+        point ? 'queen' : ''
+      }</div>
+    )
+  }</div>
 
 }
 
-export default ChessTable
